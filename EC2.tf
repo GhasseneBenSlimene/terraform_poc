36f6d3b44ad43e0efc510eb277a3694f94d725ba
Ghassene Ben Slimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2025-06-30 18:08:44 +0200

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-07d0cf3af28718ef8" # Amazon Linux 2 (region eu-west-1)
+  ami           = "ami-0fab1b527ffa9b942"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
 
