fff75660532a4f293d45312f5dc40ba5dec4fd3c
Ghassene Ben Slimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2025-07-01 09:56:46 +0200

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-01f23391a59163da9"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
 
